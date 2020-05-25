99c760603539e5b2f09d63a6c0228ab89b518526
Michael Evans (iMac) <michael.evans.us@gmail.com>
2020-05-25 18:29:30 -0500

    Initial Commit

---
+++ RESTTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RESTTest
+//
+//  Created by Michael Evans on 5/25/20.
+//  Copyright © 2020 MREink. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
